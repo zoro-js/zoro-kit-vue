546bb3b378995539f9c226b64a2ba03e04fe4cc8
Yinyga Zhang <zyy7259@gmail.com>
2016-08-20 00:01:46 +0800

    hello done

--- src/js/entry/hello.js
+++ src/js/entry/hello.js
@@ -1,8 +1,29 @@
-import 'module/shim'
+import 'module/common'
 
-new Vue({
-  el: '#demo',
+window.app = new Vue({
+  el: '#app',
   data: {
-    message: 'hello, vue'
+    message: 'Hello Vue',
+    newTodo: '',
+    todos: [
+      {text: 'Learn JavaScript'},
+      {text: 'Learn Vue'},
+      {text: 'Build something awesome'}
+    ]
+  },
+  methods: {
+    reverseMessage () {
+      this.message = this.message.split('').reverse().join('')
+    },
+    addTodo () {
+      const todo = this.newTodo.trim()
+      if (todo) {
+        this.todos.push({text: todo})
+        this.newTodo = ''
+      }
+    },
+    removeTodo (index) {
+      this.todos.splice(index, 1)
+    }
   }
 })
